52c16ec33ed63e9b8bf4f21f652cc8e9f5fc4add
Cory Thompson <coryjthompson@gmail.com>
2016-08-03 15:21:45 +1000

    Copy assets into our application space

--- src/android/vn/tungdx/MediaPicker/MediaPicker.java
+++ src/android/vn/tungdx/MediaPicker/MediaPicker.java
@@ -10,6 +10,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,7 +63,18 @@ public class MediaPicker extends CordovaPlugin {
 					.getMediaItemSelected(data);
 
 			for (int i = 0; i < mediaSelectedList.size(); i++) {
-				fileNames.add(mediaSelectedList.get(i).getPathOrigin(context).toString());
+				File inputFile = new File(mediaSelectedList.get(i).getPathOrigin(context).toString());
+				String ext = inputFile.getAbsolutePath().substring(inputFile.getAbsolutePath().lastIndexOf("."));
+				File outputFile = getWritableFile(ext);
+
+				try {
+					copyFile(inputFile, outputFile);
+				} catch (IOException exception) {
+					this.callbackContext.error(exception.getMessage());
+					return;
+				}
+
+				fileNames.add(outputFile.getAbsolutePath());
 			}
 
 			JSONArray res = new JSONArray(fileNames);
@@ -67,5 +84,35 @@ public class MediaPicker extends CordovaPlugin {
 
 		JSONArray res = new JSONArray(fileNames);
 		this.callbackContext.success(res);
+	}
+
+	private File getWritableFile(String ext) {
+		int i = 1;
+		File dataDirectory = cordova.getActivity().getApplicationContext().getFilesDir();
+
+		// Create the data directory if it doesn't exist
+		dataDirectory.mkdirs();
+		String dataPath = dataDirectory.getAbsolutePath();
+		File file;
+		do {
+			file = new File(dataPath + String.format("/capture_%05d." + ext, i));
+			i++;
+		} while (file.exists());
+
+		return file;
+	}
+
+	public void copyFile(File src, File dst) throws IOException {
+		InputStream in = new FileInputStream(src);
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
 	}
 }
