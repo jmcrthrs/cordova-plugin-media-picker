52d8e674e2fdc75351dcaa7dc9d846833dc4b5c8
root <root@ryan.dev.busivid.com>
2017-08-18 13:56:15 +1000

    Alphabetize options

--- www/cordova-plugin-multiple-media-selection.js
+++ www/cordova-plugin-multiple-media-selection.js
@@ -5,12 +5,12 @@ var MultipleMediaSelection = function(){};
 MultipleMediaSelection.prototype.getPictures = function(success, error, options) {
 	options = options || {};
 
-	options.minImages = options.minImages || 0;
+	options.height = options.height || 0;
 	options.maxImages = options.maxImages || 5;
 	options.mediaType = options.mediaType || 'any';
-	options.width = options.width || 0;
-	options.height = options.height || 0;
+	options.minImages = options.minImages || 0;
 	options.quality = options.quality || 100;
+	options.width = options.width || 0;
 
 	exec(success, error, 'MultipleMediaSelection', 'getPictures', [options]);
 };
