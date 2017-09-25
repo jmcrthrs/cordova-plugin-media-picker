ea49f8214aa5c3c05d30e61936be4fd55684315b
Jamie Carruthers <jamie@otixo.com>
2017-09-25 14:27:59 +0200

    use js prototype

--- www/MediaPicker.js
+++ www/MediaPicker.js
@@ -1,4 +1,35 @@
-var exec = require('cordova/exec');
+var MediaPicker = function() {
+	
+};
+	
+MediaPicker.prototype.getPictures = function(success, fail, options) {
+	options = options || {};
+
+	options.height = options.height || 0;
+	options.isTemporaryFile = typeof options.isTemporaryFile === 'undefined' || options.isTemporaryFile == true;
+	options.maxImages = options.maxImages || 20;
+	options.mediaType = options.mediaType || 'any';
+	options.minImages = options.minImages || 0;
+	options.progress = options.progress || function(result) {};
+	options.quality = options.quality || 100;
+	options.width = options.width || 0;
+
+	successCallback = function(result) {
+		if (typeof result !== 'undefined' && typeof result.type !== 'undefined' && typeof options.progress === 'function') {
+			options.progress(result);
+			return;
+		}
+
+		success(result);
+	}
+
+	return cordova.exec(successCallback, fail, 'MediaPicker', 'getPictures', [options]);
+};
+
+window.mediaPicker = new MediaPicker();
+
+
+/*var exec = require('cordova/exec');
 
 exports.ERROR_CANCELLED = 'CANCELLED';
 exports.PROGRESS_MEDIA_IMPORTED = 'MEDIA_IMPORTED';
@@ -40,3 +71,4 @@ exports.requestPermission = function(success, error) {
 	exec(success, error, 'MediaPicker', 'requestPermission', [options]);
 };
 
+*/
