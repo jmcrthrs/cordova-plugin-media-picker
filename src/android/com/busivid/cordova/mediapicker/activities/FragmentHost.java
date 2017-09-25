f113fd2b68757a9204e26395d4cde7eeb9449d66
Jamie Carruthers <jamie@otixo.com>
2017-09-25 14:27:47 +0200

    remove android

--- src/android/com/busivid/cordova/mediapicker/activities/FragmentHost.java
+++
@@ -1,7 +0,0 @@
-package com.busivid.cordova.mediapicker.activities;
-
-import com.busivid.cordova.mediapicker.imageloader.MediaImageLoader;
-
-public interface FragmentHost {
-	MediaImageLoader getImageLoader();
-}
--- src/android/com/busivid/cordova/mediapicker/imageloader/MediaImageLoader.java
+++
@@ -1,8 +0,0 @@
-package com.busivid.cordova.mediapicker.imageloader;
-
-import android.net.Uri;
-import android.widget.ImageView;
-
-public interface MediaImageLoader {
-	void displayImage(Uri uri, ImageView imageView);
-}
